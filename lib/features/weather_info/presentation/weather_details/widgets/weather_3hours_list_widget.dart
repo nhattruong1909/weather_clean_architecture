7ee3f622eec903f5709d2b6779a0f800c34ba150
Nguyễn Hữu Nhật Trường <nhattruong4373@gmail.com>
2024-07-25 09:53:52 +0700

    add scroll bar

--- lib/features/weather_info/presentation/weather_details/widgets/weather_3hours_list_widget.dart
+++ lib/features/weather_info/presentation/weather_details/widgets/weather_3hours_list_widget.dart
@@ -27,38 +27,41 @@ class Weather3hoursListWidget extends StatelessWidget {
         decoration: const BoxDecoration(
             color: Color(0x26FFFFFF),
             borderRadius: const BorderRadius.all(Radius.circular(20.0))),
-        child: ListView.builder(
-            // scrollDirection: Axis.horizontal,
-            itemCount: timestamps.length,
-            itemBuilder: (BuildContext context, int index) {
-              final _icon = timestamps[index].weather?[0]?.icon ?? '01d';
-              return Container(
-                  width: 40,
-                  height: 40,
-                  child: Row(children: [
-                    Expanded(
-                        flex: 2,
-                        child: Text(
-                          timestamps[index].dt ?? '',
-                          style: poppinsRegular.copyWith(fontSize: 30),
-                        )),
-                    Expanded(
-                      flex: 1,
-                      child: SizedBox(
-                          width: 40,
-                          height: 40,
-                          child: Image.network(
-                            'https://openweathermap.org/img/wn/$_icon@2x.png',
-                            fit: BoxFit.fitHeight,
+        child: Scrollbar(
+          child: ListView.builder(
+              // scrollDirection: Axis.horizontal,
+              itemCount: timestamps.length,
+              itemBuilder: (BuildContext context, int index) {
+                final _icon = timestamps[index].weather?[0]?.icon ?? '01d';
+                return Container(
+                    width: 40,
+                    height: 40,
+                    child: Row(children: [
+                      Expanded(
+                          flex: 2,
+                          child: Text(
+                            timestamps[index].dt ?? '',
+                            style: poppinsRegular.copyWith(fontSize: 30),
                           )),
-                    ),
-                    Expanded(
-                        flex: 2,
-                        child: Align(
-                            alignment: Alignment.centerRight,
-                            child: Text(timestamps[index].main?.temp ?? '',
-                                style: poppinsRegular.copyWith(fontSize: 30))))
-                  ]));
-            }));
+                      Expanded(
+                        flex: 1,
+                        child: SizedBox(
+                            width: 40,
+                            height: 40,
+                            child: Image.network(
+                              'https://openweathermap.org/img/wn/$_icon@2x.png',
+                              fit: BoxFit.fitHeight,
+                            )),
+                      ),
+                      Expanded(
+                          flex: 2,
+                          child: Align(
+                              alignment: Alignment.centerRight,
+                              child: Text(timestamps[index].main?.temp ?? '',
+                                  style:
+                                      poppinsRegular.copyWith(fontSize: 30))))
+                    ]));
+              }),
+        ));
   }
 }
